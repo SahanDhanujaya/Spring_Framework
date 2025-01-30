7d708905c16389f2d100fc2a64082d86dd1768a4
SahanDhanujaya <sahandhanujaya18@gmail.com>
2025-01-30 11:08:06 +0530

    population of propety

--- 06_Runtime_Value_Injection/src/main/java/lk/ijse/bean/SpringBean.java
+++ 06_Runtime_Value_Injection/src/main/java/lk/ijse/bean/SpringBean.java
@@ -1,17 +1,24 @@
 package lk.ijse.bean;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBean {
+public class SpringBean implements InitializingBean {
     @Value("Dhanu")
     String name;
     public SpringBean() {
         System.out.println("Spring Bean Constructor");
         System.out.println("Name : "+name);
     }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Name : "+name);
+    }
+
 
     /*@Autowired(required = false)
     public SpringBean(@Value("Dhanu") String name,@Value("1") int id ,@Value("true") boolean isTrue){
