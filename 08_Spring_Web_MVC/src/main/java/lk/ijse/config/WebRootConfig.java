c168d1f79fba8a31ece5ec779302f2cf268a009d
SahanDhanujaya <sahandhanujaya18@gmail.com>
2025-02-06 10:50:07 +0530

    ExactMapping pushed

--- 08_Spring_Web_MVC/src/main/java/lk/ijse/config/WebRootConfig.java
+++ 08_Spring_Web_MVC/src/main/java/lk/ijse/config/WebRootConfig.java
@@ -4,5 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+
 public class WebRootConfig {
 }
--- 08_Spring_Web_MVC/src/main/java/lk/ijse/controller/HelloController.java
+++ 08_Spring_Web_MVC/src/main/java/lk/ijse/controller/HelloController.java
@@ -1,22 +1,17 @@
 package lk.ijse.controller;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("hello")
 public class HelloController {
-    public HelloController() {
-        System.out.println("HelloController");
-    }
-    @GetMapping("/test1")
+    @GetMapping
     public String sayHello(){
-        return "Hello World";
+        return "index";
     }
-    @GetMapping("/test2")
-    public String test(){
-        return "Test";
-    }
+
 }
