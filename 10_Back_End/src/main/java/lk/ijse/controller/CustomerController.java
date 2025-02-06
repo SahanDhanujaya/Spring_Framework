198119b3c0ebe43c28a35f50b593221191725ebd
SahanDhanujaya <sahandhanujaya18@gmail.com>
2025-02-06 15:52:29 +0530

    today last commit

--- 10_Back_End/src/main/java/lk/ijse/controller/CustomerController.java
+++ 10_Back_End/src/main/java/lk/ijse/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/v1/customer")
 public class CustomerController {
